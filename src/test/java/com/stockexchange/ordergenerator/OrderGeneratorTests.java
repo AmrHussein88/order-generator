6fa9eb27ec465dddc3fb3cdfa2e6f6c56b6f70d7
amrhussein <eng.amr881@gmail.com>
2020-09-13 22:44:36 +0300

    removed unused dependencies and imports

--- src/test/java/com/stockexchange/ordergenerator/OrderGeneratorTests.java
+++ src/test/java/com/stockexchange/ordergenerator/OrderGeneratorTests.java
@@ -10,12 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
-import java.util.HashMap;
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations="classpath:application-test.properties")
-public class OrderGeneratorTest {
+public class OrderGeneratorTests {
 
     @Mock
     private RestTemplate restTemplate;
@@ -26,18 +24,6 @@ public class OrderGeneratorTest {
     public void generateOrder_Test(){
         Mockito.when(restTemplate.postForEntity("http://localhost:8080/orders", orderDto, OrderDto.class))
         .thenReturn(new ResponseEntity<>(orderDto, HttpStatus.OK));
-    }
-
-    @Test
-    public void generateOrder_TestIT(){
-        Map<String, String> json = new HashMap<>();
-        json.put("symbol", "APPLE");
-        json.put("quantity", "2");
-//        RestAssured.with().body(json).when()
-//                .request("POST", "http://localhost:8080/orders").then().statusCode(200);
-//      Response body =  RestAssured.given().contentType(ContentType.JSON).body(json)
-//                .when().post("http://localhost:8080/orders").then().extract().response();
-//        Assert.assertEquals(200, body.getStatusCode());
     }
 
 }
